d27804cd1da0d6f00589096f8a858b6d46046f07
philsawa <philsawa@gmail.com>
2020-09-03 13:14:29 -0400

    main.js

--- scripts/Helloworldmain.js
+++ scripts/Helloworldmain.js
@@ -1,2 +1,10 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello, World!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/81202016010.jpg') {
+    myImage.setAttribute('src','images/mollieandellie.jpg');
+  } else {
+    myImage.setAttribute('src','images/81202016010.jpg');
+  }
+}
