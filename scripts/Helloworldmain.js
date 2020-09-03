696c9d37a3b2bb401a04f8dbd4dd537eaafa0a2d
philsawa <philsawa@gmail.com>
2020-09-03 15:33:59 -0400

    update Helloworldmain.js

--- scripts/Helloworldmain.js
+++ scripts/Helloworldmain.js
@@ -11,3 +11,19 @@ myImage.onclick = function() {
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Molzilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Molzilla is cool, ' + storedName;
+}
+
+myButton.onclick function() {
+  setUserName();
+}
