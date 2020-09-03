26fe31d96714d8d90184a2207870aa2ed2d52203
philsawa <philsawa@gmail.com>
2020-09-03 13:29:05 -0400

    update Helloworldmain.js

--- scripts/Helloworldmain.js
+++ scripts/Helloworldmain.js
@@ -3,7 +3,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
   if(mySrc === 'images/81202016010.jpg') {
-    myImage.setAttribute('src','images/mollieandellie.jpg');
+    myImage.setAttribute('src','images/mollieandellie2.jpg');
   } else {
     myImage.setAttribute('src','images/81202016010.jpg');
   }
