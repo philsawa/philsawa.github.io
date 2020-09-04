680ae6132c102a940d470bdf2d65e5d34c58485a
philsawa <philsawa@gmail.com>
2020-09-04 12:04:25 -0400

    update Helloworldmain.js

--- scripts/Helloworldmain.js
+++ scripts/Helloworldmain.js
@@ -14,7 +14,7 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
-  if (!myName) {
+  if(!myName) {
     setUserName();
   } else {
   localStorage.setItem('name', myName);
